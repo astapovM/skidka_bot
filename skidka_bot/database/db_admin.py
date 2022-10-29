98c071d477e33e518425af5636e626c508d65a4b
astapovM <sustaff@mail.ru>
2022-10-29 12:47:12 +0300

    Добавлению ссылки на товар в БД

--- skidka_bot/database/db_admin.py
+++ skidka_bot/database/db_admin.py
@@ -9,7 +9,7 @@ def sql_start():
         "CREATE TABLE IF NOT EXISTS users(user_id PRIMARY KEY, user_name TEXT, connect_date )"
     )
     cur.execute(
-        "CREATE TABLE IF NOT EXISTS packages(id PRIMARY KEY, package_url TEXT, old_price FLOAT, "
+        "CREATE TABLE IF NOT EXISTS packages(id , package_url TEXT, old_price FLOAT, "
         "new_price FLOAT, amount INT)"
     )
     base.commit()
@@ -19,5 +19,5 @@ def check_user_in_db(user_id):
     cur.execute(f"SELECT * FROM users WHERE user_id={user_id}")
     return cur.fetchone()
 
-def add_package_url():
-    cur.execute()
+def add_package_url(user_id, data):
+    cur.execute("""INSERT INTO packages(id, package_url) VALUES(?,?)""",f"({user_id},{data['url']})")
