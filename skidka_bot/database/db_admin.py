0cc8a4fb34630d53311fc879c93e6d238eb6b045
astapovM <sustaff@mail.ru>
2022-10-31 18:54:53 +0300

    Проверка добавленных товаров для пользователя

--- skidka_bot/database/db_admin.py
+++ skidka_bot/database/db_admin.py
@@ -6,10 +6,10 @@ def sql_start():
     base = sqlite3.connect('database/skidka.db')
     cur = base.cursor()
     cur.execute(
-        "CREATE TABLE IF NOT EXISTS users(user_id PRIMARY KEY, user_name TEXT, connect_date )"
+        "CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, user_id , user_name TEXT, connect_date, FOREIGN KEY (id) REFERENCES packages (user_id) )"
     )
     cur.execute(
-        "CREATE TABLE IF NOT EXISTS packages(id , package_url TEXT, old_price FLOAT, "
+        "CREATE TABLE IF NOT EXISTS packages(id INTEGER PRIMARY KEY AUTOINCREMENT, user_id, package_url, old_price FLOAT, "
         "new_price FLOAT, amount INT)"
     )
     base.commit()
@@ -19,5 +19,8 @@ def check_user_in_db(user_id):
     cur.execute(f"SELECT * FROM users WHERE user_id={user_id}")
     return cur.fetchone()
 
-def add_package_url(user_id, data):
-    cur.execute("""INSERT INTO packages(id, package_url) VALUES(?,?)""",f"({user_id},{data['url']})")
+
+def check_packages(user_id):
+    cur.execute(f"SELECT package_url FROM packages WHERE user_id ={user_id}")
+    return cur.fetchall()
+
