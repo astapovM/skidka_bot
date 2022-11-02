cfdac16ad1c379a8f8c96f7019f517fd627344a0
astapovM <sustaff@mail.ru>
2022-11-02 17:19:13 +0300

    Кнопка скидки

--- skidka_bot/buttons/keyboard_button.py
+++ skidka_bot/buttons/keyboard_button.py
@@ -1,7 +1,10 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-start_button1 = InlineKeyboardButton('🔗 Отправить ссылку на товар', callback_data='url_button')
-start_button2 = InlineKeyboardButton('📦 Посмотреть мои товары', callback_data='package_button')
-start_button3 = InlineKeyboardButton('ℹ️ Помощь', callback_data='help_button')
+start_button1 = InlineKeyboardButton('🔗 Отправить ссылку на товар 🔗', callback_data='url_button')
+check_items = InlineKeyboardButton('📦 Посмотреть мои товары 📦', callback_data='package_button')
+help_button = InlineKeyboardButton('ℹ️ Помощь ℹ️', callback_data='help_button')
+delete_button = InlineKeyboardButton('🗑 Удалить товар из списка отслеживаемых ️🗑️', callback_data='delete_button')
+personal_sale_button = InlineKeyboardButton('%💲 Ввести свою персональную скидку 💲%', callback_data='personal_sale_button')
 
-inline_start_kb = InlineKeyboardMarkup().add(start_button1).add(start_button2).add(start_button3)
+inline_start_kb = InlineKeyboardMarkup().add(start_button1).add(check_items).add(personal_sale_button).add(delete_button).add(help_button)
+
