1a3aedae6a411618e1d45f680951eb7083fb77dc
astapovM <sustaff@mail.ru>
2022-11-08 13:39:43 +0300

    Класс для fsm состояний

--- skidka_bot/states/set_states.py
+++ skidka_bot/states/set_states.py
@@ -1,6 +1,6 @@
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
-
+#Сохранение ответов пользователя в fsm
 class Url_input(StatesGroup):
     insert_url = State()
     insert_item_id = State()
